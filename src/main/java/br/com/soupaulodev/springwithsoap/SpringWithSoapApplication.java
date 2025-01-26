c3d60c9d90c5f3c0c9829ff5508132239cc6cc87
Paulo Marques <paulomarques.rs1@gmail.com>
2025-01-26 10:15:05 -0300

    Initial commit

---
+++ src/main/java/br/com/soupaulodev/springwithsoap/SpringWithSoapApplication.java
@@ -0,0 +1,13 @@
+package br.com.soupaulodev.springwithsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithSoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/soupaulodev/springwithsoap/SpringWithSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.soupaulodev.springwithsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
