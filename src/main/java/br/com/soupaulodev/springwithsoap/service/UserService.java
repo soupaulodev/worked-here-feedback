01127e31486599d205db4b1f82ff8765dca7dec8
Paulo Marques <paulomarques.rs1@gmail.com>
2025-02-07 01:25:48 -0300

    feat: Implement logging for UserService methods
    Adds logging to the UserService methods to track user
    creation, retrieval, update, and deletion, including
    warnings for potential errors.

--- src/main/java/br/com/soupaulodev/springwithsoap/service/UserService.java
+++ src/main/java/br/com/soupaulodev/springwithsoap/service/UserService.java
@@ -7,6 +7,8 @@ import br.com.soupaulodev.springwithsoap.mapper.UserMapper;
 import br.com.soupaulodev.springwithsoap.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -16,12 +18,19 @@ import java.util.UUID;
 @Transactional
 public class UserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     public CreateUserResponse createUser(CreateUserRequest request) {
+        logger.info("Receive request to create new user");
+
         userRepository.findByUsernameOrEmail(request.getUsername(), request.getEmail())
-                .orElseThrow(() -> new RuntimeException("Username or Email already exists"));
+                .ifPresent((user) -> {
+                    logger.warn("Can't create new user. Username or email already exists");
+                    throw new RuntimeException("Username or Email already exists");
+                });
 
         UserEntity userEntity = userMapper.createRequestToEntity(request);
         userEntity.setId(UUID.randomUUID());
@@ -32,18 +41,24 @@ public class UserService {
         CreateUserResponse response = new CreateUserResponse();
         response.setUserId(userEntity.getId().toString());
         response.setOperationStatus(OperationStatus.SUCESS.toString());
+        logger.info("New user created with id: {}", userEntity.getId());
 
         return response;
     }
 
     public GetUserInfoResponse getUserInfo(UUID userId) {
         UserEntity userEntity = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> {
+                    logger.warn("User with id {} not found", userId);
+                    return new RuntimeException("User not found");
+                });
+
 
         User user = userMapper.entityToTypeObj(userEntity);
 
         GetUserInfoResponse response = userMapper.typeObjToGetUserInfoResponse(user);
         response.setOperationStatus(OperationStatus.SUCESS.toString());
+        logger.info("User with id {} found successfully", userId);
 
         return response;
     }
@@ -51,11 +66,15 @@ public class UserService {
     public UpdateUserResponse updateUser(UpdateUserRequest request) {
         if (request.getEmail() == null || request.getUsername() == null
                 || request.getPassword() == null || request.getActive() == null) {
+            logger.warn("All fields are null. Can't update");
             throw new IllegalArgumentException("All fields are null");
         }
 
         UserEntity userEntity = userRepository.findById(UUID.fromString(request.getUserId()))
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> {
+                    logger.warn("User with id {} not found to update", request.getUserId());
+                    return new RuntimeException("User not found");
+                });
 
         userEntity.setEmail(request.getEmail() != null ? request.getEmail() : userEntity.getEmail());
         userEntity.setUsername(request.getUsername() != null ? request.getUsername() : userEntity.getUsername());
@@ -69,18 +88,23 @@ public class UserService {
         UpdateUserResponse response = new UpdateUserResponse();
         response.setOperationStatus("SUCESS");
 
+        logger.info("User with id {} updated", userEntity.getId());
         return response;
     }
 
     public DeleteUserResponse deleteUser(UUID userId) {
         UserEntity userEntity = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> {
+                    logger.warn("User with id {} not found", userId);
+                    return new RuntimeException("User not found");
+                });
 
         userRepository.delete(userEntity);
 
         DeleteUserResponse response = new DeleteUserResponse();
         response.setOperationStatus(OperationStatus.SUCESS.toString());
 
+        logger.info("User with id {} deleted", userEntity.getId());
         return response;
     }
 }
